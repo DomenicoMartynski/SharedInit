a23b785700ddfb342943619e09fa247b2d3593ee
DomenicoMartyński <domisku123@gmail.com>
2025-05-15 20:29:54 +0200

    Quick fix 3

--- toggle_windows_autostart.bat
+++ toggle_windows_autostart.bat
@@ -13,19 +13,7 @@ if exist "%SHORTCUT_FILE%" (
     echo Autostart has been disabled. The shortcut has been removed from the Windows Startup folder.
 ) else (
     :: Shortcut does not exist, so enable autostart by creating it
-    echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
-    echo sLinkFile = "%SHORTCUT_FILE%" >> CreateShortcut.vbs
-    echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
-    echo oLink.TargetPath = """"%SCRIPT_DIR%SharedInit - Windows Launcher.bat"""" >> CreateShortcut.vbs
-    echo oLink.WorkingDirectory = """"%SCRIPT_DIR%"""" >> CreateShortcut.vbs
-    echo oLink.Description = "SharedInit LAN File Sharing" >> CreateShortcut.vbs
-    echo oLink.WindowStyle = 7 >> CreateShortcut.vbs
-    echo oLink.Save >> CreateShortcut.vbs
-
-    :: Run the VBS script
-    cscript //nologo CreateShortcut.vbs
-    del CreateShortcut.vbs
-
+    cscript //nologo CreateShortcut.vbs "%SHORTCUT_FILE%" "%SCRIPT_DIR%SharedInit - Windows Launcher.bat" "%SCRIPT_DIR%"
     echo Autostart has been enabled. A shortcut has been created in the Windows Startup folder.
 )
 
